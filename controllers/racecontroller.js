791bf714372188214d3f84d8604ba55aadea20bd
Rhys <redwards717@gmail.com>
2018-07-03 23:03:40 -0400

    commit for presentation

--- controllers/racecontroller.js
+++ controllers/racecontroller.js
@@ -190,6 +190,133 @@ router.get('/find/:id', function (req, res) {
 router.put('/update', function (req, res) {
     var data = req.body.race.id;
 
+    var start = Number(req.body.race.starters);
+    var fin = Number(req.body.race.place);
+    var type = req.body.race.race_type;
+    var added;
+
+    if (type == 'Criterium') {
+        if (start >= 50) {
+            switch (fin) {
+                case 1:
+                    added = 7
+                    break;
+                case 2:
+                    added = 6
+                    break;
+                case 3:
+                    added = 5
+                    break;
+                case 4:
+                    added = 4
+                    break;
+                case 5:
+                    added = 3
+                    break;
+                case 6:
+                    added = 2
+                    break;
+                case 7:
+                    added = 1
+                    break;
+                default:
+                    added = 0
+            }
+        } else if (start >= 21) {
+            switch (fin) {
+                case 1:
+                    added = 5
+                    break;
+                case 2:
+                    added = 4
+                    break;
+                case 3:
+                    added = 3
+                    break;
+                case 4:
+                    added = 2
+                    break;
+                case 5:
+                    added = 1
+                    break;
+                default:
+                    added = 0
+            }
+        } else if (start >= 11) {
+            switch (fin) {
+                case 1:
+                    added = 4
+                    break;
+                case 2:
+                    added = 3
+                    break;
+                case 3:
+                    added = 2
+                    break;
+                case 4:
+                    added = 1
+                    break;
+                default:
+                    added = 0
+            }
+        } else if (start >= 5) {
+            switch (fin) {
+                case 1:
+                    added = 3
+                    break;
+                case 2:
+                    added = 2
+                    break;
+                case 3:
+                    added = 1
+                    break;
+                case 4:
+                default:
+                    added = 0
+            }
+        } else {
+            added = 0
+        }
+    } else if (type == 'Road Race') {
+        if (start >= 50) {
+            if (fin == 1) {
+                added = 10
+            } else{
+                added = 10 - fin;
+                if (added < 0) {
+                    added = 0;
+                }
+            }
+        } else if (start >= 21) {
+            if (fin == 1) {
+                added = 8
+            } else {
+                added = 8 - fin;
+                if (added < 0) {
+                    added = 0;
+                }
+            }
+        } else if (start >= 11) {
+            if (fin == 1) {
+                added = 7
+            } else {
+                added = 7 - fin;
+                if (added < 0) {
+                    added = 0;
+                }
+            }
+        } else if (start >= 5) {
+            added = 4 - fin;
+            if (added < 0) {
+                added = 0;
+            }
+        } else {
+            return;
+        }
+    } else if (type == 'Time Trial') {
+        added = 0
+    }
+
     
     Race.update({
         race_name: req.body.race.race_name,
@@ -197,7 +324,8 @@ router.put('/update', function (req, res) {
         category: req.body.race.category,
         starters: req.body.race.starters,
         place: req.body.race.place,
-        date: req.body.race.date
+        date: req.body.race.date,
+        points: added
     },
         { where: { id: data } }
     ).then(
@@ -209,6 +337,7 @@ router.put('/update', function (req, res) {
                 starters: req.body.race.starters,
                 place: req.body.race.place,
                 date: req.body.race.date,
+                points: added,
                 message: 'race info has been updated'
             });
         },
